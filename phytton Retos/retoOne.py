dcbf39f669f86effecb180e98959b0cd79d36875
Jose28782 <125732071+Jose28782@users.noreply.github.com>
2023-03-11 08:08:03 -0500

    Add files via upload

---
+++ phytton Retos/retoOne.py
@@ -0,0 +1,6 @@
+print ("Hola abu ")
+
+gradof= float (input("Ingresa los grados los fahrenheit" ))
+
+gradosC= (gradof-32)/1.8
+gradosC= float (print("el valor en grados Centigrador es de ",gradosC))
---
+++ phytton Retos/retoTwo.py
@@ -0,0 +1,10 @@
+print ("hola enfermera")
+
+pesobb= float(input("Ingresa el peso de el bebe: " ))
+total1 = pesobb + 10
+mesesbb=float(input("Ingrasa los meses de nacido del bebe: "))
+total2 = mesesbb * 10 
+total1_2 = total1/total2
+
+dosis = total1_2 * 8
+dosisVacuna = print ("bb =",dosis)
